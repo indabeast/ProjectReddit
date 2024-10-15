efc75b107b2d48a75baf6ed024bad043e5892aed
hendrikmolgaard <Moelgaard01@hotmail.com>
2024-10-15 19:02:00 +0200

    Your commit message

--- ProRedClient/obj/Debug/net7.0/ProRedClient.AssemblyInfo.cs
+++ ProRedClient/obj/Debug/net7.0/ProRedClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProRedClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e7dc5793d3b6bdee125d9cf8674eaf93c44b28")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProRedClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProRedClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
